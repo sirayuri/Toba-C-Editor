0ea5dce40509ba96f8381e237dcc51f53b2a634b
sirayuri <118947615+sirayuri@users.noreply.github.com>
2023-03-09 00:11:17 +0900

    Add files via upload

---
+++ Hackathon/toba-c-editer/toba-c-editor.Designer.cs
@@ -0,0 +1,325 @@
+﻿namespace toba_c_editer
+{
+    partial class Create_New
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Create_New));
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.ファイルFToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.開くToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.名前を付けて保存ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.上書き保存ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem2 = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.表示ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.フルスクリーンモードToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.拡大ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.縮小ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.ターミナルToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.新しいターミナルToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("MS UI Gothic", 17F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.button1.Location = new System.Drawing.Point(382, 30);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(151, 32);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "コンパイル";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("MS UI Gothic", 17F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.button2.Location = new System.Drawing.Point(553, 31);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(151, 32);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "実行";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox3
+            // 
+            this.textBox3.AcceptsReturn = true;
+            this.textBox3.BackColor = System.Drawing.SystemColors.InactiveCaptionText;
+            this.textBox3.CharacterCasing = System.Windows.Forms.CharacterCasing.Lower;
+            this.textBox3.Font = new System.Drawing.Font("MS UI Gothic", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.textBox3.ForeColor = System.Drawing.SystemColors.InactiveBorder;
+            this.textBox3.Location = new System.Drawing.Point(0, 758);
+            this.textBox3.Multiline = true;
+            this.textBox3.Name = "textBox3";
+            this.textBox3.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.textBox3.Size = new System.Drawing.Size(1924, 271);
+            this.textBox3.TabIndex = 5;
+            this.textBox3.Text = "started toba-c-editor";
+            // 
+            // ファイルFToolStripMenuItem
+            // 
+            this.ファイルFToolStripMenuItem.BackColor = System.Drawing.Color.Gray;
+            this.ファイルFToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.開くToolStripMenuItem,
+            this.名前を付けて保存ToolStripMenuItem,
+            this.上書き保存ToolStripMenuItem,
+            this.toolStripMenuItem1,
+            this.toolStripMenuItem2});
+            this.ファイルFToolStripMenuItem.ForeColor = System.Drawing.Color.White;
+            this.ファイルFToolStripMenuItem.Name = "ファイルFToolStripMenuItem";
+            this.ファイルFToolStripMenuItem.Size = new System.Drawing.Size(53, 20);
+            this.ファイルFToolStripMenuItem.Text = "ファイル";
+            // 
+            // 開くToolStripMenuItem
+            // 
+            this.開くToolStripMenuItem.BackColor = System.Drawing.Color.Gray;
+            this.開くToolStripMenuItem.ForeColor = System.Drawing.Color.White;
+            this.開くToolStripMenuItem.Name = "開くToolStripMenuItem";
+            this.開くToolStripMenuItem.Size = new System.Drawing.Size(161, 22);
+            this.開くToolStripMenuItem.Text = "開く";
+            this.開くToolStripMenuItem.Click += new System.EventHandler(this.開くToolStripMenuItem_Click);
+            // 
+            // 名前を付けて保存ToolStripMenuItem
+            // 
+            this.名前を付けて保存ToolStripMenuItem.BackColor = System.Drawing.Color.Gray;
+            this.名前を付けて保存ToolStripMenuItem.ForeColor = System.Drawing.Color.White;
+            this.名前を付けて保存ToolStripMenuItem.Name = "名前を付けて保存ToolStripMenuItem";
+            this.名前を付けて保存ToolStripMenuItem.Size = new System.Drawing.Size(161, 22);
+            this.名前を付けて保存ToolStripMenuItem.Text = "名前を付けて保存";
+            this.名前を付けて保存ToolStripMenuItem.Click += new System.EventHandler(this.名前を付けて保存ToolStripMenuItem_Click);
+            // 
+            // 上書き保存ToolStripMenuItem
+            // 
+            this.上書き保存ToolStripMenuItem.BackColor = System.Drawing.Color.Gray;
+            this.上書き保存ToolStripMenuItem.ForeColor = System.Drawing.Color.White;
+            this.上書き保存ToolStripMenuItem.Name = "上書き保存ToolStripMenuItem";
+            this.上書き保存ToolStripMenuItem.Size = new System.Drawing.Size(161, 22);
+            this.上書き保存ToolStripMenuItem.Text = "上書き保存";
+            this.上書き保存ToolStripMenuItem.Click += new System.EventHandler(this.上書き保存ToolStripMenuItem_Click);
+            // 
+            // toolStripMenuItem1
+            // 
+            this.toolStripMenuItem1.BackColor = System.Drawing.Color.Gray;
+            this.toolStripMenuItem1.ForeColor = System.Drawing.Color.White;
+            this.toolStripMenuItem1.Name = "toolStripMenuItem1";
+            this.toolStripMenuItem1.Size = new System.Drawing.Size(161, 22);
+            this.toolStripMenuItem1.Text = "新規ファイル";
+            this.toolStripMenuItem1.Click += new System.EventHandler(this.toolStripMenuItem1_Click);
+            // 
+            // toolStripMenuItem2
+            // 
+            this.toolStripMenuItem2.BackColor = System.Drawing.Color.Gray;
+            this.toolStripMenuItem2.ForeColor = System.Drawing.Color.White;
+            this.toolStripMenuItem2.Name = "toolStripMenuItem2";
+            this.toolStripMenuItem2.Size = new System.Drawing.Size(161, 22);
+            this.toolStripMenuItem2.Text = "閉じる";
+            this.toolStripMenuItem2.Click += new System.EventHandler(this.toolStripMenuItem2_Click);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.BackColor = System.Drawing.Color.Gray;
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(24, 24);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.ファイルFToolStripMenuItem,
+            this.表示ToolStripMenuItem,
+            this.ターミナルToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1924, 24);
+            this.menuStrip1.TabIndex = 6;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // 表示ToolStripMenuItem
+            // 
+            this.表示ToolStripMenuItem.BackColor = System.Drawing.Color.Gray;
+            this.表示ToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.フルスクリーンモードToolStripMenuItem,
+            this.拡大ToolStripMenuItem,
+            this.縮小ToolStripMenuItem});
+            this.表示ToolStripMenuItem.ForeColor = System.Drawing.Color.White;
+            this.表示ToolStripMenuItem.Name = "表示ToolStripMenuItem";
+            this.表示ToolStripMenuItem.Size = new System.Drawing.Size(43, 20);
+            this.表示ToolStripMenuItem.Text = "表示";
+            // 
+            // フルスクリーンモードToolStripMenuItem
+            // 
+            this.フルスクリーンモードToolStripMenuItem.BackColor = System.Drawing.Color.Gray;
+            this.フルスクリーンモードToolStripMenuItem.CheckOnClick = true;
+            this.フルスクリーンモードToolStripMenuItem.ForeColor = System.Drawing.Color.White;
+            this.フルスクリーンモードToolStripMenuItem.Name = "フルスクリーンモードToolStripMenuItem";
+            this.フルスクリーンモードToolStripMenuItem.Size = new System.Drawing.Size(160, 22);
+            this.フルスクリーンモードToolStripMenuItem.Text = "フルスクリーンモード";
+            this.フルスクリーンモードToolStripMenuItem.Click += new System.EventHandler(this.フルスクリーンモードToolStripMenuItem_Click);
+            // 
+            // 拡大ToolStripMenuItem
+            // 
+            this.拡大ToolStripMenuItem.BackColor = System.Drawing.Color.Gray;
+            this.拡大ToolStripMenuItem.ForeColor = System.Drawing.Color.White;
+            this.拡大ToolStripMenuItem.Name = "拡大ToolStripMenuItem";
+            this.拡大ToolStripMenuItem.Size = new System.Drawing.Size(160, 22);
+            this.拡大ToolStripMenuItem.Text = "拡大";
+            this.拡大ToolStripMenuItem.Click += new System.EventHandler(this.拡大ToolStripMenuItem_Click);
+            // 
+            // 縮小ToolStripMenuItem
+            // 
+            this.縮小ToolStripMenuItem.BackColor = System.Drawing.Color.Gray;
+            this.縮小ToolStripMenuItem.ForeColor = System.Drawing.Color.White;
+            this.縮小ToolStripMenuItem.Name = "縮小ToolStripMenuItem";
+            this.縮小ToolStripMenuItem.Size = new System.Drawing.Size(160, 22);
+            this.縮小ToolStripMenuItem.Text = "縮小";
+            this.縮小ToolStripMenuItem.Click += new System.EventHandler(this.縮小ToolStripMenuItem_Click);
+            // 
+            // ターミナルToolStripMenuItem
+            // 
+            this.ターミナルToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.新しいターミナルToolStripMenuItem});
+            this.ターミナルToolStripMenuItem.ForeColor = System.Drawing.Color.White;
+            this.ターミナルToolStripMenuItem.Name = "ターミナルToolStripMenuItem";
+            this.ターミナルToolStripMenuItem.Size = new System.Drawing.Size(62, 20);
+            this.ターミナルToolStripMenuItem.Text = "ターミナル";
+            // 
+            // 新しいターミナルToolStripMenuItem
+            // 
+            this.新しいターミナルToolStripMenuItem.BackColor = System.Drawing.Color.Gray;
+            this.新しいターミナルToolStripMenuItem.ForeColor = System.Drawing.Color.White;
+            this.新しいターミナルToolStripMenuItem.Name = "新しいターミナルToolStripMenuItem";
+            this.新しいターミナルToolStripMenuItem.Size = new System.Drawing.Size(147, 22);
+            this.新しいターミナルToolStripMenuItem.Text = "新しいターミナル";
+            this.新しいターミナルToolStripMenuItem.Click += new System.EventHandler(this.新しいターミナルToolStripMenuItem_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.Cursor = System.Windows.Forms.Cursors.Arrow;
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.Font = new System.Drawing.Font("MS UI Gothic", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "日本語 Shift-JIS",
+            "日本語 EUC",
+            "日本語 JIS",
+            "Unicode",
+            "UTF8",
+            "UTF-32"});
+            this.comboBox1.Location = new System.Drawing.Point(1636, 3);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(276, 21);
+            this.comboBox1.TabIndex = 8;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.White;
+            this.label1.Font = new System.Drawing.Font("MS UI Gothic", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label1.Location = new System.Drawing.Point(42, 41);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 22);
+            this.label1.TabIndex = 9;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(12, 32);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(78, 27);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "label2";
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.AcceptsTab = true;
+            this.richTextBox1.Font = new System.Drawing.Font("Courier New", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.richTextBox1.Location = new System.Drawing.Point(0, 69);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(1924, 686);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = "";
+            this.richTextBox1.TextChanged += new System.EventHandler(this.richTextBox1_TextChanged);
+            // 
+            // Create_New
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(14F, 27F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.ClientSize = new System.Drawing.Size(1924, 1050);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.menuStrip1);
+            this.Font = new System.Drawing.Font("MS UI Gothic", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.KeyPreview = true;
+            this.Margin = new System.Windows.Forms.Padding(6, 7, 6, 7);
+            this.Name = "Create_New";
+            this.Text = "toba-c-editor";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Create_New_FormClosing);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Create_New_KeyDown);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.ToolStripMenuItem ファイルFToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 開くToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 名前を付けて保存ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 上書き保存ToolStripMenuItem;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem2;
+        private System.Windows.Forms.ToolStripMenuItem 表示ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem ターミナルToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem フルスクリーンモードToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 拡大ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 縮小ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 新しいターミナルToolStripMenuItem;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+    }
+}
